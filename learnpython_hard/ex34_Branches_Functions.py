4fbf63c560f447a17950c73ea3e78b3bb336aec0
vthaver <36577263+vthaver@users.noreply.github.com>
2018-03-06 07:58:24 -0500

    cdeeeceece:wq :wq :wq 

--- learnpython_hard/ex34_Branches_Functions.py
+++ learnpython_hard/ex34_Branches_Functions.py
@@ -2,43 +2,45 @@
 
 from sys import exit
 
-def old_room():
-	print("THis room is full of gold, How much do you take?")
+def gold_room():
+	print("This room is full of gold, How much do you take?")
 	
 	choice = input('> ')
 	if "0" in choice or "1" in choice:
-		how_much = int(choice)
+		how_much = float(choice)
 	else:
 		dead("Man, learn to type a number.") #Prints the string + god job, then exits the ....
 	
 	if how_much < 50:
-		print("Nice, you'r not greedy, you win!")
+		print("Nice, you're not greedy, you win!")
 		exit(0)
 	else:
 		dead("You greedy bastard!")
-	
-Def bear_room():
+		
+
+def bear_room():
 	print("There is a bear here.")
 	print("The bear has a bunch of honey.")
 	print("The fat bear is in front of another door.")
 	print("How are you going to move the bear?")
-	bear_moved = false
+	bear_moved = False #Initializes the bear as not being moved, so hat some action below will change that
 	
 	while True:
 		choice = input('> ')
 		
 		if choice == "take honey":
 			dead("The bear looks at you then slaps your face off.")
-		elif choice == "taunt bear" and not bear_moved:
+		elif choice == "taunt bear" and not bear_moved: #If you taunt the bear and the bear is not moved (Bear_moved = False)
 			print("The bear has moved from the door.")
 			print("You can go through it now")
 			bear_moved = True
 		elif choice == "taunt bear" and bear_moved:
-			dead("THe bear gets pissed off and chews your leg off.")
+			dead("The bear gets pissed off and chews your leg off.")
 		elif choice == "open door" and bear_moved:
 			gold_room()
 		else:
 			print("I got no idea what that means.")
+
 
 def cthulhu_room():
 	print("Here you see the great evil Cthulhu.")
@@ -54,24 +56,24 @@ def cthulhu_room():
 	else:
 		cthulu_room()
 
-def dead(why)
-	print(why, "Good Job!")
-	exit(0)
+def dead(why):
+	print(why, "Good Job!") #Prints the argument passed into the initialization of the functions
+	exit(0) #Exits the program? Python
 	
 def start():
 	print("You are in a dark room")
-	print("THere is a door to you right and a door to your left")
+	print("There is a door to you right and a door to your left")
 	print("Which door do you take?")
 	
 	choice = input('> ')
 	
 	if choice == "left":
-		bear_room()
+		bear_room() # Run the bear function
 	elif choice == "right":
-		cthulhu_room()
+		cthulhu_room() #Run the cthuhlu function  
 	else:
-		dead("You stumble around the room until you starve.")
+		dead("You stumble around the room until you starve.") #Run the dead function, which prints something and exits the program
 		
-start()
+start() #run the function which eventually leads to running the rest of the functions
 	
 			
